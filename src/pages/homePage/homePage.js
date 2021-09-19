fe596a77d19f680a4a68eba74b381123368be3a7
Erik Mkrtchyan <kire45@gmail.com>
2021-09-19 16:40:44 +0300

    restaurant names editted

--- src/pages/homePage/homePage.js
+++ src/pages/homePage/homePage.js
@@ -1,18 +1,67 @@
-const makeid = (length) => {
-    let result           = '';
-    let characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    let charactersLength = characters.length;
-    for ( let i = 0; i < length; i++ ) {
-        result += characters.charAt(Math.floor(Math.random() *
-            charactersLength));
-    }
-    return result;
-}
+
 
 let randomInteger = (min, max) => {
     // получить случайное число от (min-0.5) до (max+0.5)
     let rand = min - 0.5 + Math.random() * (max - min + 1);
     return Math.round(rand);
+}
+
+const makeid = () => {
+    let restNames = ["Cheesecake Factory",
+        "Shokolaat",
+        "Gordon Biersch",
+        "Crepevine",
+        "Creamery",
+        "Old Pro",
+        "House of Bagels",
+        "The Prolific Oven",
+        "La Strada",
+        "Buca di Beppo",
+        "Madame Tam",
+        "Sprout Cafe",
+        "Junoon",
+        "Bistro Maxine",
+        "Three Seasons",
+        "Reposado",
+        "Siam Royal",
+        "Krung Siam",
+        "Thaiphoon",
+        "Tamarine",
+        "Joya",
+        "Jing Jing",
+        "Evvia Estiatorio",
+        "Cafe 220",
+        "Cafe Renaissance",
+        "Kan Zeman",
+        "Mango Caribbean Cafe",
+        "Baklava",
+        "Mandarin Gourmet",
+        "Bangkok Cuisine",
+        "Darbar Indian Cuisine",
+        "Mantra",
+        "Janta",
+        "Hyderabad House",
+        "Starbucks",
+        "Coupa Cafe",
+        "Lytton Coffee Company",
+        "Il Fornaio",
+        "Lavanda",
+        "MacArthur Park",
+        "Osteria",
+        "Vero",
+        "Cafe Renzo",
+        "Miyake",
+        "Sushi Tomo",
+        "Kanpai",
+        "Pizza My Heart",
+        "New York Pizza",
+        "California Pizza Kitchen",
+        "Round Table",
+        "Loving Hut",
+        "Garden Fresh",
+        "Cafe Epi",
+        "Tai Pan"];
+    return restNames[randomInteger(0, restNames.length - 1)]
 }
 
 const restaurantList = {restaurantList:
@@ -30,7 +79,7 @@ const restaurantList = {restaurantList:
 for (let i = 0; i < 100; ++i) {
     let random = {
         imgUrl: 'https://st.depositphotos.com/2075661/2208/i/600/depositphotos_22080415-stock-photo-kfc-logo.jpg',
-        name: makeid(1121),
+        name: makeid(),
         cost: randomInteger(300, 500).toString(),
         minDeliveryTime: randomInteger(10, 15).toString(),
         maxDeliveryTime: randomInteger(30, 55).toString(),
