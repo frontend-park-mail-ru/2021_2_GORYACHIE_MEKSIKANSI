14b7100bdb441d5530c35814ef8da88db1488318
Erik Mkrtchyan <kire45@gmail.com>
2021-09-21 01:20:48 +0300

    working on auth

---
+++ src/events/Auth.js
@@ -0,0 +1,6 @@
+export const AuthStatus = {
+    userLogin: 'userSignIn',
+    userLogout: 'userLogout',
+    offline: 'offlineRequest',
+    notAuth: 'notAuth',
+}
---
+++ src/modules/auth.js
@@ -0,0 +1,12 @@
+import { AuthStatus } from "../events/Auth.js";
+import eventBus from "./eventBus.js";
+
+export function auth(response) {
+    if (response.status !== 200) {
+        eventBus.emitEventListener(AuthStatus.notAuth, {});
+        console.log('user is not logged in', response.status)
+    } else {
+        eventBus.emitEventListener(AuthStatus.userLogin, response.parsedJSON);
+        console.log('user is logged in', response.status, response.parsedJSON)
+    }
+}
