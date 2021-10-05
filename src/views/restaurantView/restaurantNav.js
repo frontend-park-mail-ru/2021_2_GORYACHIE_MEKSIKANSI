184fb9b183bac7f073ad0ef77d1bc26b289dd226
Sergey <falkr4m@yandex.ru>
2021-10-06 00:38:26 +0300

    Start connect elements in page

---
+++ src/views/restaurantView/restaurantNav.js
@@ -0,0 +1,23 @@
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {
+  stickNavbar();
+};
+
+// Get the navbar
+const navbar = document.querySelector('.restaurant-nav__list');
+
+// Get the offset position of the navbar
+const sticky = navbar.offsetTop;
+
+const stickNavbar = () => {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add('sticky');
+    navbar.style.left = '0';
+    navbar.style.borderBottom = 'solid 1px #e2e2e2';
+    navbar.style.paddingLeft = '3%';
+  } else {
+    navbar.classList.remove('sticky');
+    navbar.style.borderBottom = '';
+    navbar.style.paddingLeft = '0';
+  }
+};
