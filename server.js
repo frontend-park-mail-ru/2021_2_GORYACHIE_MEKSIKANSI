f67746f26a3c6de7d544441055b1dd3a9aa7da24
Sergey Scherbakov <falkr4m@yandex.ru>
2021-10-12 16:20:07 +0300

    Fixed bug with cart not stopping on the end of page

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ app.use(express.static('src/'));
 app.use('/profile', express.static(path.resolve(__dirname, '', 'src/')));
 app.use('/login', express.static(path.resolve(__dirname, '', 'src/')));
 app.use('/signup', express.static(path.resolve(__dirname, '', 'src/')));
+app.use('/restaurants/1', express.static(path.resolve(__dirname, '', 'src/')));
 
 app.all('*', (request, response) => {
   response.sendFile(path.resolve(__dirname, '', 'src/'));  // TODO: придумать что-то с отдачей статики, поднять nginx
