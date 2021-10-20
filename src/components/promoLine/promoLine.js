99949793cbea903383dcc523cb0a1235554bb346
Sergey <falkr4m@yandex.ru>
2021-10-20 03:02:03 +0300

    Added flexible promoline with hiding button

--- src/components/promoLine/promoLine.js
+++ src/components/promoLine/promoLine.js
@@ -1,10 +1,12 @@
 
 let begin = 0;
-let blockWidth = document.querySelector('.promo-line').offsetWidth;
 
 // console.log(document.querySelector('.promo-line').offsetLeft);
 
+let blockWidth = document.querySelector('.promo-line').offsetWidth;
+
 const getNextRightScrollCoors = () => {
+  blockWidth = document.querySelector('.promo-line').offsetWidth;
   const blocks = document.querySelectorAll('.promo-block');
   const lastBlock = [...blocks].find((block) => {
     return block.offsetLeft + block.offsetWidth - block.parentNode.offsetLeft > begin + blockWidth;
@@ -13,27 +15,28 @@ const getNextRightScrollCoors = () => {
   if (lastBlock) {
     begin = lastBlock.offsetLeft - lastBlock.parentNode.offsetLeft;
   }
+  console.log(begin);
   return begin;
 };
 
 const getNextLeftScrollCoors = () => {
+  blockWidth = document.querySelector('.promo-line').offsetWidth;
   const blocks = document.querySelectorAll('.promo-block');
   const firstBlock = [...blocks].find((block) => {
     return block.offsetLeft + block.offsetWidth - block.parentNode.offsetLeft > begin;
   });
-  console.log(firstBlock);
   if (firstBlock) {
     begin = firstBlock.offsetLeft - firstBlock.parentNode.offsetLeft;
   }
+  console.log(begin);
   return begin;
 };
 
 const turnLeft = () => {
-  if (begin > blockWidth) {
-    begin -= blockWidth;
-  }
   if (begin < blockWidth) {
     begin = 0;
+  } else if (begin > blockWidth) {
+    begin -= blockWidth;
   }
   document.querySelector('.promo-line').scrollTo({
     left: getNextLeftScrollCoors(),
