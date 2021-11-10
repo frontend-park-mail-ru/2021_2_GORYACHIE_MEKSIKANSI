a7f6d7ce1658cd30b0483eaeaeefb58e278718a6
Erik Mkrtchyan <kire45@gmail.com>
2021-11-10 10:25:37 +0300

    main deleted

---
+++ dist/main.1095ec0b4043926f77b2.hot-update.js
@@ -0,0 +1,29 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdate_2021_2_GORYACHIE_MEKSIKANSI"]("main",{
+
+/***/ "./src/components/mapPopup/mapPopup.js":
+/*!*********************************************!*\
+  !*** ./src/components/mapPopup/mapPopup.js ***!
+  \*********************************************/
+/***/ (() => {
+
+eval("throw new Error(\"Module parse failed: Unexpected token (97:12)\\nYou may need an appropriate loader to handle this file type, currently no loaders are configured to process this file. See https://webpack.js.org/concepts#loaders\\n|             .removeEventListener('click', this.addAddress)\\n| \\n>         if ()\\n|     }\\n| \");\n\n//# sourceURL=webpack://2021_2_GORYACHIE_MEKSIKANSI/./src/components/mapPopup/mapPopup.js?");
+
+/***/ })
+
+},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ /* webpack/runtime/getFullHash */
+/******/ (() => {
+/******/ 	__webpack_require__.h = () => ("1dba3bbbe9e3496cd87a")
+/******/ })();
+/******/ 
+/******/ }
+);
