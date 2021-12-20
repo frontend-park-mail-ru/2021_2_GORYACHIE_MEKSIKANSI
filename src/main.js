15744cf6ac360d01ae9700464c6785be812a936d
Erik Mkrtchyan <kire45@gmail.com>
2021-12-20 17:26:19 +0300

    linter fixes

--- src/main.js
+++ src/main.js
@@ -24,14 +24,14 @@ const routeTo = (url) => {
 };
 
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/sw.js', { scope: './' })
+  navigator.serviceWorker.register('/sw.js', {scope: './'})
       .then((registration) => {
         const data = {
           type: 'CACHE_URLS',
           payload: [
             location.href,
-            ...performance.getEntriesByType('resource').map((r) => r.name)
-          ]
+            ...performance.getEntriesByType('resource').map((r) => r.name),
+          ],
         };
         registration.installing.postMessage(data);
       })
